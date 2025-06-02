8d6ea15b23b3c553cbe8c1e9b6ee6ab04cbaeaec
Christopher Timario <christopher.timario@accenture.com>
2025-06-02 10:11:44 +0800

    Fix build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,10 +13,10 @@ const config: Config = {
       },
       keyframes: {
         fadeInOut: {
-          '0%': { opacity: 0 },
-          '10%': { opacity: 1 },
-          '90%': { opacity: 1 },
-          '100%': { opacity: 0 },
+          '0%': { opacity: '0' },
+          '10%': { opacity: '1' },
+          '90%': { opacity: '1' },
+          '100%': { opacity: '0' },
         },
       },
       screens: {
